91a116dd45d13103ace23ba32ddb4cd8be675c91
ArtmMtvnko <artemm3note@gmail.com>
2026-05-02 13:47:02 +0300

    Fixed bug with imports

--- lib/decks/index.ts
+++ lib/decks/index.ts
@@ -2,14 +2,15 @@ export {
   createDeck,
   getDeckForEditing,
   getDeckForLearning,
+  getUserUnpublishedDecks,
   recordReview,
   updateDeck,
 } from './deck.service'
-export {
-  getUserPublishedDecks,
-  getUserUnpublishedDecks,
-  publishDeck,
-} from './public-deck.service'
+export { getUserPublishedDecks, publishDeck } from './public-deck.service'
 export { createDeckSchema, updateDeckSchema } from './deck.schemas'
 export type { CreateDeckInput, UpdateDeckInput } from './deck.schemas'
-export type { DeckSummary, DeckWithFlashcards, FlashcardWithSrs } from './deck.types'
+export type {
+  DeckSummary,
+  DeckWithFlashcards,
+  FlashcardWithSrs,
+} from './deck.types'
